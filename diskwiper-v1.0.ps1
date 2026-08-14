12119a71531ce572c2047e28c2bd5e66e6706d1c
Jack McCartney <167548653+jack-mccartney-cae@users.noreply.github.com>
2026-08-14 17:12:46 +0100

    Update Disk Wiper script to version 2.0

--- diskwiper-v1.0.ps1
+++ diskwiper-v1.0.ps1
@@ -1,5 +1,8 @@
+Write-Host "Starting Disk Wiper Cloud v2.0"
 $Serial = (Get-CimInstance Win32_BIOS).SerialNumber
+Write-Host "Device Serial = $Serial"
 $Model = (Get-CimInstance Win32_ComputerSystem).Model
+Write-Host "Device Model = $Model"
 
 [PSCustomObject]@{
     DateTime = Get-Date
@@ -7,8 +10,13 @@ $Model = (Get-CimInstance Win32_ComputerSystem).Model
     Model = $Model
 } | Export-Csv ".\WipeLog.csv" -Append -NoTypeInformation
 
+Write-Host "Proceeding to wipe in 10 Seconds..."
+
 Start-Sleep -Seconds 10
 
+Write-Host "Now Wiping..."
 Clear-LocalDisk -Force -Confirm:$false
 
+Write-Host "Device with Serial ($Serial) has been wiped at ($DateTime) and has been logged"
+Write-Host "Script ending in 20 seconds..."
 Start-Sleep -Seconds 20
