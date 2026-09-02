592361ca6c93eac69c6b1c6988450defdcaec2aa
Jack McCartney <167548653+jack-mccartney-cae@users.noreply.github.com>
2026-09-02 15:06:55 +0100

    Update diskwiper-v2.0.ps1

--- diskwiper-v2.0.ps1
+++ diskwiper-v2.0.ps1
@@ -3,16 +3,15 @@ Write-Host "Starting Disk Wiper Cloud v3.0"
 Write-Host "Device Details:"
 $Serial = (Get-CimInstance Win32_BIOS).SerialNumber
 Write-Host "Device Serial = $Serial"
+$Model = (Get-CimInstance Win32_ComputerSystem).Manufacturer
+Write-Host "Device Manufacturer = $Manufacturer"
 $Model = (Get-CimInstance Win32_ComputerSystem).Model
 Write-Host "Device Model = $Model"
 $Date = Get-Date
 Write-Host "Current Date and Time = $Date"
 
-[PSCustomObject]@{
-    DateTime = Get-Date
-    SerialNumber = $Serial
-    Model = $Model
-} | Export-Csv "e:\WipeLog.csv" -Append -NoTypeInformation
+$USBDrive = (Get-Volume | Where-Object FileSystemLabel -eq "OSDCloud").DriveLetter
+$LogFile = "$($USBDrive):\WipeLog.csv"
 
 Write-Host "Proceeding to wipe in 10 Seconds..."
 
@@ -20,6 +19,14 @@ Start-Sleep -Seconds 10
 
 Write-Host "Now Wiping..."
 Clear-LocalDisk -Force -Confirm:$false
+Write-Host "Wipe Complete"
+
+[PSCustomObject]@{
+    DateTime = Get-Date
+    Manufacturer = $Manufacturer
+    Model = $Model
+    SerialNumber = $Serial
+} | Export-Csv $LogFile -NoTypeInformation
 
 Write-Host "Device with Serial ($Serial) has been wiped at ($Date) and has been logged"
 Write-Host "Script ending in 20 seconds..."
